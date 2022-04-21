3ecca3c053387fbf0e12c13bfd2198b0075ac625
jereediv <102240284+jereediv@users.noreply.github.com>
2022-04-21 13:34:43 -0500

    added list of candidate options

--- PyPoll.py
+++ PyPoll.py
@@ -4,14 +4,12 @@ import os
    # Module for reading CSV files
 import csv
 
-csvpath = "election_results.csv"
-#csvpath = "..\Class Folder\Election_Analysis\election_results.csv"
-
-
 # Create a filename variable to a direct or indirect path
+csvpath = "election_results.csv"
 
+#csvpath = "..\Class Folder\Election_Analysis\election_results.csv"
 #csvpath = os.path.join("analysis", "election_analysis.txt")
-total_votes = 0
+
 with open(csvpath) as csvfile:
     print(csvfile)
     #CSV reader specifies delimiter and variable that holds contents
@@ -43,20 +41,25 @@ with open(file_to_save, "w") as txt_file:
    # Read and analize data
 
 # 1. Initialize a total vote counter.
-
+total_votes = 0
+candidate_options = []
    # Open the election results and read the file
 with open(csvpath) as election_data:
    file_reader = csv.reader(election_data)
    headers = next(file_reader)
-   # Print each row in the CSV file
+   # Print each row in the CSV file  
    for row in file_reader:
       # 2. Add the total vote count.
       total_votes += 1
    # 3. Print the total votes.
-print("Total votes", + total_votes)
-
-
-
+      #print(total_votes)
+# Print the candidate name from each row
+      candidate_name = row[2]
+# If the candidate does not match an existing candidate..            
+      if candidate_name not in candidate_options:
+         # Add it to the list of candidates.
+         candidate_options.append(candidate_name)
+print(candidate_options)
 
 
 
