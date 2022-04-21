035c1ad522b129649535fd00856f99013d6cb96f
jereediv <102240284+jereediv@users.noreply.github.com>
2022-04-21 12:49:04 -0500

    Updated with total votes

--- PyPoll.py
+++ PyPoll.py
@@ -4,12 +4,14 @@ import os
    # Module for reading CSV files
 import csv
 
-csvpath = ("election_results.csv")
+csvpath = "election_results.csv"
 #csvpath = "..\Class Folder\Election_Analysis\election_results.csv"
+
+
 # Create a filename variable to a direct or indirect path
 
 #csvpath = os.path.join("analysis", "election_analysis.txt")
-
+total_votes = 0
 with open(csvpath) as csvfile:
     print(csvfile)
     #CSV reader specifies delimiter and variable that holds contents
@@ -18,10 +20,9 @@ with open(csvpath) as csvfile:
     #Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-    # Read each row of the data after the header
-    #for row in csvreader:
-        #print(row)
-   
+    
+    # Read each row of the data after the header  
+
 # Import the datetime class from the datetime module
 
 import datetime as dt
@@ -40,8 +41,19 @@ with open(file_to_save, "w") as txt_file:
 # Write three counties to the file
    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
    # Read and analize data
-   
 
+# 1. Initialize a total vote counter.
+
+   # Open the election results and read the file
+with open(csvpath) as election_data:
+   file_reader = csv.reader(election_data)
+   headers = next(file_reader)
+   # Print each row in the CSV file
+   for row in file_reader:
+      # 2. Add the total vote count.
+      total_votes += 1
+   # 3. Print the total votes.
+print("Total votes", + total_votes)
 
 
 
